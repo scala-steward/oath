a19eb8feea954f327ab63e70b8a10533319426ad
Andreas Rigas <48296471+andrewrigas@users.noreply.github.com>
2022-11-13 23:37:23 +0000

    Publish (#1)

--- project/ContinuesIntegration.scala
+++
@@ -1,5 +0,0 @@
-import org.typelevel.sbt.gha.GenerativeKeys.{githubWorkflowGeneratedCacheSteps, githubWorkflowJavaVersions}
-import org.typelevel.sbt.gha.{UseRef, WorkflowJob, WorkflowStep}
-import sbt.Keys._
-
-object ContinuesIntegration {}
